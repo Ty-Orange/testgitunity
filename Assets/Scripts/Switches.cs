372cdce234c6b435b29ed31b07fc3b26e5c17321
Ty-Orange <d.ortiz.aguilera@gmail.com>
2021-10-20 09:32:54 +0200

    Initial_Project_Setup

---
+++ Assets/Scripts/Switches.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches
+{
+    public static bool debugMode = true;
+    public static bool debugShowIds = true;
+    public static bool debugShowEnemyFollow = true;
+    public static bool debugTurboMode = true;
+    public static bool debugDialogs = true;
+}
